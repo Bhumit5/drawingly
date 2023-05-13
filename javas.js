e988ebed68b8daea8e0fd226288a85df5a9c7981
Bhumit5 <106863660+Bhumit5@users.noreply.github.com>
2023-05-13 07:25:58 +0530

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,11 @@
+// document.querySelector('body').addEventListener('mousemove', eyeball);
+
+// function eyeball() {
+//     var eye = document.querySelector('.eye');
+//     eye.forEach(function(eye) {
+//         let x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
+//         let y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
+//         let radian = Math.atan2(event.pageX - x, event.pageY - y);
+
+//     });
+// }
